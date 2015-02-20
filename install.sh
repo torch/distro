torch/distro

commit ba196a20d6a7d80ff5c6ed59e9a872d0954db776

adding DYLD_LIBRARY_PATH

install.sh
@@ -116,6 +116,7 @@ add the following lines to your shell profile:
 
 export PATH=$PREFIX/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH 
+export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH 
 "
 fi
 
@@ -154,10 +155,12 @@ if [[ $WRITE_PATH_TO_PROFILE == 1 ]]; then
 
 export PATH=$PREFIX/bin:\$PATH  # Added automatically by torch-dist
 export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist" >> $RC_FILE
+export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH  # Added automatically by torch-dist" >> $RC_FILE
     echo "
 
 export PATH=$PREFIX/bin:\$PATH  # Added automatically by torch-dist
 export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist" >> $HOME/.profile
+export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH  # Added automatically by torch-dist" >> $HOME/.profile
 
 else
     echo "
@@ -168,5 +171,6 @@ add the following lines to your shell profile:
 
 export PATH=$PREFIX/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH 
+export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH 
 "
 fi

commit 0a2d44b795e5f2183916c3d5273541daa9a62fdf

Fixed bug regarding DYLD_LIBRARY_PATH.
The DYLD_LIBRARYPATH is not added to the bashrc because the echoed string
is already closed on the previous line. Problem introduced in earlier
commit (ba196a20d6a7d80ff5c6ed59e9a872d0954db776).

install.sh
@@ -154,12 +154,12 @@ if [[ $WRITE_PATH_TO_PROFILE == 1 ]]; then
     echo "
 
 export PATH=$PREFIX/bin:\$PATH  # Added automatically by torch-dist
-export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist" >> $RC_FILE
+export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist
 export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH  # Added automatically by torch-dist" >> $RC_FILE
     echo "
 
 export PATH=$PREFIX/bin:\$PATH  # Added automatically by torch-dist
-export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist" >> $HOME/.profile
+export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH  # Added automatically by torch-dist
 export DYLD_LIBRARY_PATH=$PREFIX/lib:\$DYLD_LIBRARY_PATH  # Added automatically by torch-dist" >> $HOME/.profile
 
 else