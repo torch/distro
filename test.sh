2deb8f6de7d62b78f98f8c29662193a4289d1eaa
soumith <soumith@fb.com>
2015-08-05 04:55:46 -0700

    adding smoke and unit testing now for both LuaJIT and Lua51/Lua52

--- test.sh
+++ test.sh
@@ -1,30 +1,37 @@
+LUA=$(which luajit)
+LUA=${LUA:-"lua"}
+echo $LUA
+
 #smoke tests
-luajit -lpaths     -e "print('paths loaded succesfully')"
-luajit -ltorch     -e "print('torch loaded succesfully')"
-luajit -lenv       -e "print('env loaded succesfully')"
-luajit -ltrepl     -e "print('trepl loaded succesfully')"
-luajit -ldok       -e "print('dok loaded succesfully')"
-luajit -limage     -e "print('image loaded succesfully')"
-luajit -lsundown   -e "print('sundown loaded succesfully')"
-luajit -lcwrap     -e "print('cwrap loaded succesfully')"
-luajit -lgnuplot   -e "print('gnuplot loaded succesfully')"
-luajit -loptim     -e "print('optim loaded succesfully')"
-luajit -lsys       -e "print('sys loaded succesfully')"
-luajit -lxlua      -e "print('xlua loaded succesfully')"
-luajit -largcheck  -e "print('argcheck loaded succesfully')"
-luajit -laudio     -e "print('audio loaded succesfully')"
-luajit -lfftw3     -e "print('fftw3 loaded succesfully')"
-luajit -lgraph     -e "print('graph loaded succesfully')"
-luajit -lnn        -e "print('nn loaded succesfully')"
-luajit -lnngraph   -e "print('nngraph loaded succesfully')"
-luajit -lnnx       -e "print('nnx loaded succesfully')"
-luajit -lgraphicsmagick -e "print('graphicsmagick loaded succesfully')"
-luajit -lsdl2      -e "print('sdl2 loaded succesfully')"
-luajit -lsignal    -e "print('signal loaded succesfully')"
-luajit -lthreads   -e "print('threads loaded succesfully')"
+$LUA -lpaths     -e "print('paths loaded succesfully')"
+$LUA -ltorch     -e "print('torch loaded succesfully')"
+$LUA -lenv       -e "print('env loaded succesfully')"
+$LUA -ltrepl     -e "print('trepl loaded succesfully')"
+$LUA -ldok       -e "print('dok loaded succesfully')"
+$LUA -limage     -e "print('image loaded succesfully')"
+$LUA -lcwrap     -e "print('cwrap loaded succesfully')"
+$LUA -lgnuplot   -e "print('gnuplot loaded succesfully')"
+$LUA -loptim     -e "print('optim loaded succesfully')"
+$LUA -lsys       -e "print('sys loaded succesfully')"
+$LUA -lxlua      -e "print('x$LUA loaded succesfully')"
+$LUA -largcheck  -e "print('argcheck loaded succesfully')"
+$LUA -lgraph     -e "print('graph loaded succesfully')"
+$LUA -lnn        -e "print('nn loaded succesfully')"
+$LUA -lnngraph   -e "print('nngraph loaded succesfully')"
+$LUA -lnnx       -e "print('nnx loaded succesfully')"
+$LUA -lthreads   -e "print('threads loaded succesfully')"
 
 th -ltorch -e "torch.test()"
 th -lnn    -e "nn.test()"
+
+if [ $LUA == 'luajit' ];
+then
+    $LUA -lsundown   -e "print('sundown loaded succesfully')"
+    $LUA -lsignal    -e "print('signal loaded succesfully')"
+    $LUA -lgraphicsmagick -e "print('graphicsmagick loaded succesfully')"
+    $LUA -lfftw3     -e "print('fftw3 loaded succesfully')"
+    $LUA -laudio     -e "print('audio loaded succesfully')"
+fi
 
 
 # CUDA tests
@@ -33,9 +40,12 @@ path_to_nvidiasmi=$(which nvidia-smi)
 
 if [ -x "$path_to_nvcc" ] || [ -x "$path_to_nvidiasmi" ]
 then
-    luajit -lcutorch -e "print('cutorch loaded succesfully')"
-    luajit -lcunn -e "print('cunn loaded succesfully')"
-    luajit -lcudnn -e "print('cudnn loaded succesfully')"
+    $LUA -lcutorch -e "print('cutorch loaded succesfully')"
+    $LUA -lcunn -e "print('cunn loaded succesfully')"
+    if [ $LUA == 'luajit' ];
+    then
+        $LUA -lcudnn -e "print('cudnn loaded succesfully')"
+    fi
     th -lcutorch -e "cutorch.test()"
     th -lcunn -e "nn.testcuda()"
 fi
