d6377fbd6a38e0b6e0e2999727a01db58df6bf8d
Boris Fomitchev <bfomitchev@nvidia.com>
2016-02-03 01:39:19 -0800

    Preparing for pbuild

--- install.sh
+++ install.sh
@@ -70,8 +70,8 @@ mkdir -p ${BUILD_DIR}
 if [[ "$TORCH_LUA_VERSION" == "NATIVE" ]]; then
 echo "Using NATIVE Lua version:"
 
-# export LUAROCKS="luarocks --tree=$PREFIX $VERBOSE"
-export LUAROCKS="luarocks $VERBOSE"
+export LUAROCKS="luarocks --tree=$PREFIX $VERBOSE"
+# export LUAROCKS="luarocks $VERBOSE"
 # temporaruily, until all the rocks are fixed
 # we are exporting variables needed for correct location of includes here
 export LUAJIT_INCDIR=/usr/include/luajit-2.0
