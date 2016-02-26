bb54115976d29ae047b0754f54f0d98fbb446675
Boris Fomitchev <bfomitchev@nvidia.com>
2016-02-25 21:41:09 -0800

    updated tree

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
-git pull
-git submodule update
+# git pull
+# git submodule update
+git submodule foreach git fetch
 git submodule foreach git pull origin master
+git submodule foreach git checkout master
+git submodule foreach git pull
 git add extra pkg exe
 git commit -m "updating packages"
