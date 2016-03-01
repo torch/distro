1ce74bd6836a7439ffe72fe48f5f574d96a0de5e
Boris Fomitchev <bfomitchev@nvidia.com>
2016-02-29 22:11:46 -0800

    fixed CPATH

--- install.sh
+++ install.sh
@@ -178,28 +178,15 @@ cd ${THIS_DIR}/extra/audio          && $LUAROCKS make audio-0.1-0.rockspec || ex
 cd ${THIS_DIR}/extra/fftw3          && $LUAROCKS make rocks/fftw3-scm-1.rockspec || exit 1
 cd ${THIS_DIR}/extra/signal         && $LUAROCKS make rocks/signal-scm-1.rockspec || exit 1
 
+#Support for Protobuf
 cd ${THIS_DIR}/extra/lua-pb         && $LUAROCKS make lua-pb-scm-0.rockspec || exit 1
+# Lua Wrapper for LMDB, latest from github (lightningmdb)
 cd ${THIS_DIR}/extra/lmdb           && $LUAROCKS make LMDB_INCDIR=/usr/include LMDB_LIBDIR=/usr/lib/x86_64-linux-gnu lightningmdb-scm-1.rockspec || exit 1
 cd ${THIS_DIR}/extra/totem          && $LUAROCKS make rocks/totem-0-0.rockspec || exit 1
-cd ${THIS_DIR}/extra/hdf5           && $LUAROCKS make hdf5-0-0.rockspec || exit 1
-cd ${THIS_DIR}/extra/nccl         && $LUAROCKS make nccl-scm-1.rockspec || exit 1
-cd ${THIS_DIR}/extra/         && $LUAROCKS make rocks/ || exit 1
-
-#Support for Protobuf
-# ${LUAROCKS} install "https://raw.githubusercontent.com/Neopallium/lua-pb/master/lua-pb-scm-0.rockspec" || exit 1
-
-# Lua Wrapper for LMDB, latest from github (lightningmdb)
-# ${LUAROCKS} install https://raw.githubusercontent.com/shmul/lightningmdb/master/lightningmdb-scm-1.rockspec LMDB_INCDIR=/usr/include LMDB_LIBDIR=/usr/lib/x86_64-linux-gnu
-
-# ${LUAROCKS} install lightningmdb
-
 #HDF5 filesystem support
-
-# ${LUAROCKS} install "https://raw.github.com/deepmind/torch-totem/master/rocks/totem-0-0.rockspec" || exit 1
-# ${LUAROCKS} install "https://raw.github.com/deepmind/torch-hdf5/master/hdf5-0-0.rockspec"  || exit 1
-
+cd ${THIS_DIR}/extra/hdf5           && $LUAROCKS make hdf5-0-0.rockspec || exit 1
 #NCCL (experimental) support
-# ${LUAROCKS} install "https://raw.githubusercontent.com/ngimel/nccl.torch/master/nccl-scm-1.rockspec" || exit 1
+cd ${THIS_DIR}/extra/nccl         && $LUAROCKS make nccl-scm-1.rockspec || exit 1
 
 # Optional CUDA packages
 if [ -x "$path_to_nvcc" ]
@@ -226,7 +213,7 @@ $setup_lua_env_cmd
 export PATH=$PREFIX/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/local/lib:\$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/local/lib:\$DYLD_LIBRARY_PATH
-export LUA_CPATH="$PREFIX/local/lib/?.so;${LUA_CPATH}"
+export LUA_CPATH="$PREFIX/lib/?.so;$PREFIX/local/lib/?.so;${LUA_CPATH}"
 EOF
 chmod +x $PREFIX/bin/torch-activate
 
