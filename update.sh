10ed6e2844f342d23c7c3e1a3493d6afc40297aa
Boris Fomitchev <bfomitchev@nvidia.com>
2016-03-15 16:34:20 -0700

    Updated modules

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ git pull
 git submodule foreach git fetch
 git submodule foreach git checkout master
 git submodule foreach git pull
-git add extra pkg exe
-git commit -m "updating packages"
+# git add extra pkg exe
+# git commit -m "updating packages"
