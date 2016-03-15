c9e3b04b30be0a9a5720adc6c8b9638e1475ec26
Boris Fomitchev <bfomitchev@nvidia.com>
2016-03-15 16:19:42 -0700

    Updated cudnn

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 # git pull
 # git submodule update
+git pull
 git submodule foreach git fetch
-git submodule foreach git pull origin master
 git submodule foreach git checkout master
 git submodule foreach git pull
 git add extra pkg exe
