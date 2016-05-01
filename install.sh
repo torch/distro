a7daeef3f24d253054453b7542a072c999a3f5fe
Hugh Perkins <hughperkins@gmail.com>
2016-05-01 13:06:35 +0100

    update install.sh to pass in cmake_cxx_flags, so we can build cutorch on ubuntu16.04

--- install.sh
+++ install.sh
@@ -106,8 +106,8 @@ cd ${THIS_DIR}/pkg/optim     && $PREFIX/bin/luarocks make optim-1.0.5-0.rockspec
 if [ -x "$path_to_nvcc" ]
 then
     echo "Found CUDA on your machine. Installing CUDA packages"
-    cd ${THIS_DIR}/extra/cutorch && $PREFIX/bin/luarocks make rocks/cutorch-scm-1.rockspec || exit 1
-    cd ${THIS_DIR}/extra/cunn    && $PREFIX/bin/luarocks make rocks/cunn-scm-1.rockspec    || exit 1
+    cd ${THIS_DIR}/extra/cutorch && CMAKE_CXX_FLAGS=-D_FORCE_INLINES $PREFIX/bin/luarocks make rocks/cutorch-scm-1.rockspec || exit 1
+    cd ${THIS_DIR}/extra/cunn    && CMAKE_CXX_FLAGS=-D_FORCE_INLINES $PREFIX/bin/luarocks make rocks/cunn-scm-1.rockspec    || exit 1
 fi
 
 # Optional packages
@@ -128,8 +128,8 @@ cd ${THIS_DIR}/extra/signal         && $PREFIX/bin/luarocks make rocks/signal-sc
 if [ -x "$path_to_nvcc" ]
 then
     echo "Found CUDA on your machine. Installing optional CUDA packages"
-    cd ${THIS_DIR}/extra/cudnn   && $PREFIX/bin/luarocks make cudnn-scm-1.rockspec
-    cd ${THIS_DIR}/extra/cunnx   && $PREFIX/bin/luarocks make rocks/cunnx-scm-1.rockspec
+    cd ${THIS_DIR}/extra/cudnn   && CMAKE_CXX_FLAGS=-D_FORCE_INLINES  $PREFIX/bin/luarocks make cudnn-scm-1.rockspec
+    cd ${THIS_DIR}/extra/cunnx   && CMAKE_CXX_FLAGS=-D_FORCE_INLINES  $PREFIX/bin/luarocks make rocks/cunnx-scm-1.rockspec
 fi
 
 export PATH=$OLDPATH # Restore anaconda distribution if we took it out.
