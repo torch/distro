25a3a63aa93f6134323f409946686889f9e7b706
Soumith Chintala <soumith@gmail.com>
2017-05-21 13:57:30 -0400

    fix script to not update luajit-rocks

--- admin/updateSubmodules.sh
+++ admin/updateSubmodules.sh
@@ -1,5 +1,7 @@
 git pull
 git submodule update
-git submodule foreach git pull origin master
+# dont update luajit-rocks because of https://github.com/LuaJIT/LuaJIT/issues/325
+git submodule foreach bash -c 'if [ $(basename $(pwd)) != 'luajit-rocks' ]; then git pull origin master; fi'
+# git submodule foreach git pull origin master
 git add extra pkg exe
 git commit -m "updating packages"
